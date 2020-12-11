958c7cfee66078ae643379729a2191c1717ea9c3
William Funchal <williamfunchal@outlook.com>
2020-12-11 16:38:35 -0300

    Categoria working good.

--- Cadastrar_Lojas/views.py
+++ Cadastrar_Lojas/views.py
@@ -26,6 +26,8 @@ class CategoriaList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        
+
 class CategoriaDetail(APIView):    
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
@@ -40,13 +42,6 @@ class CategoriaDetail(APIView):
         categoria = self.get_object(pk)
         serializer = CategoriaSerializer(categoria)
         return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = CategoriaSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk, format=None):
         categoria = self.get_object(pk)
@@ -67,59 +62,4 @@ class CategoriaDetail(APIView):
     def delete(self, request, pk, format=None):
         categoria = self.get_object(pk)
         categoria.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class LojaList(APIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, format=None):
-        get_data = request.query_params
-        loja = Loja.objects.all()
-        if 'loja' in get_data:
-            loja = loja.filter(loja=get_data.get('loja'))        
-        serializer = LojaSerializer(loja, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = LojaSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class LojaDetail(APIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self, pk):
-        try:
-            return Loja.objects.get(pk=pk)
-        except Loja.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        loja = self.get_object(pk)
-        serializer = LojaSerializer(loja)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        loja = self.get_object(pk)
-        serializer = LojaSerializer(loja, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request, pk, format=None):
-        loja = self.get_object(pk)
-        serializer = LojaSerializer(loja, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        loja = self.get_object(pk)
-        loja.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
